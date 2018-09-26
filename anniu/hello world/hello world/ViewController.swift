087338be6e5e0c111ff398ec4ad8111ef26956a4
20161104574 <a20161104574@admindeiMac-12.local>
2018-09-26 11:21:52 +0800

    jisuan
    Jisuan

---
+++ anniu/hello world/hello world/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  hello world
+//
+//  Created by 20161104574 on 2018/9/26.
+//  Copyright © 2018年 20161104574. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func gg(_ sender: Any) {
+    }
+    @IBAction func ff(_ sender: Any) {
+    }
+    @IBAction func ee(_ sender: Any) {
+    }
+    @IBAction func dd(_ sender: Any) {
+    }
+    @IBAction func cc(_ sender: Any) {
+    }
+    @IBAction func bb(_ sender: Any) {
+    }
+    @IBAction func aa(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+
+}
+
