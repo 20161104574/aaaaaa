5413a7be835d7ad3502373ad40c7b68acff09adf
20161104574 <a20161104574@admindeiMac-12.local>
2018-10-10 10:21:09 +0800

    s

--- anniu/hello world/hello world/ViewController.swift
+++ anniu/hello world/hello world/ViewController.swift
@@ -9,21 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func gg(_ sender: Any) {
-    }
-    @IBAction func ff(_ sender: Any) {
-    }
-    @IBAction func ee(_ sender: Any) {
-    }
-    @IBAction func dd(_ sender: Any) {
-    }
-    @IBAction func cc(_ sender: Any) {
-    }
-    @IBAction func bb(_ sender: Any) {
-    }
-    @IBAction func aa(_ sender: Any) {
-    }
-    
+   
     override func viewDidLoad() {
         
         super.viewDidLoad()
